4a2a8293d5d4705a0e79f8e8bdb278a22eed1b19
kodyclemens <kody.clemens@gmail.com>
2019-02-20 14:36:01 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,14 +21,14 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
-  end
-
-  def post_data
-    post = Post.find(params[:id])
-    render plain: post.description
   end
 
 private
